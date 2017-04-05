997c67a7b16226265f659b9608a1ea27f37c2bf1
Amit Serper <aserper@gmail.com>
2017-04-05 11:22:54 -0400

    Update plumber.py
    Added license.

--- plumber.py
+++ plumber.py
@@ -8,6 +8,21 @@ import sys
 
 print "Welcome to Plumber, a grep friendly execve/fork monitor for Linux!"
 print "Written by Amit Serper, Cybereason | Contact: @0xAmit"
+#     Copyright (C) 2017, Cybereason
+# 
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as
+#     published by the Free Software Foundation, either version 3 of the
+#     License, or (at your option) any later version.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+# 
+#     You should have received a copy of the GNU Affero General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 #TODO: divide this into different classes and tidy up the code
 
